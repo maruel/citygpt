b95428ec15ddfe655a4627f5478fce90f923dd72
Marc-Antoine Ruel <maruel@gmail.com>
2025-05-03 12:41:33 -0400

    Add minimal cerebras use

--- cmd/citygpt/main.go
+++ cmd/citygpt/main.go
@@ -1,5 +1,39 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/maruel/genai"
+	"github.com/maruel/genai/cerebras"
+)
+
+func mainImpl() error {
+	c, err := cerebras.New("", "llama-3.1-8b")
+	if err != nil {
+		return err
+	}
+	msgs := genai.Messages{
+		genai.NewTextMessage(genai.User, "Is Ottawa great?"),
+	}
+	opts := genai.ChatOptions{
+		Seed:        1,
+		Temperature: 0.01,
+		MaxTokens:   50,
+	}
+	ctx := context.Background()
+	resp, err := c.Chat(ctx, msgs, &opts)
+	if err != nil {
+		return err
+	}
+	println(resp.Contents[0].Text)
+	return nil
+}
+
 func main() {
-	println("Hi!")
+	if err := mainImpl(); err != nil {
+		fmt.Fprintf(os.Stderr, "citygpt: %s\n", err)
+		os.Exit(1)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,19 @@
 module github.com/maruel/citygpt
 
 go 1.24.2
+
+require github.com/maruel/genai v0.0.0-20250503150733-b8ce0a3f732d
+
+require (
+	github.com/andybalholm/brotli v1.1.1 // indirect
+	github.com/bahlo/generic-list-go v0.2.0 // indirect
+	github.com/buger/jsonparser v1.1.1 // indirect
+	github.com/invopop/jsonschema v0.13.0 // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/mailru/easyjson v0.9.0 // indirect
+	github.com/maruel/httpjson v0.3.0 // indirect
+	github.com/maruel/roundtrippers v0.2.1 // indirect
+	github.com/wk8/go-ordered-map/v2 v2.1.8 // indirect
+	golang.org/x/sync v0.12.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
