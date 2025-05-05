30e749fb93fd1c0b309a997fa4eed54c10efb0af
Marc-Antoine Ruel <maruel@gmail.com>
2025-05-05 07:39:08 -0400

    Relicense to AGPL v3
    People that want to host it are welcome to ask for a license.

--- citygpt.go
+++ citygpt.go
@@ -1,5 +1,5 @@
 // Copyright 2025 Marc-Antoine Ruel and Félix Lachapelle. All rights reserved.
-// Use of this source code is governed under the Apache License, Version 2.0
+// Use of this source code is governed under the AGPL v3
 // that can be found in the LICENSE file.
 
 // Package citygpt includes comment embeds the data for Ottawa's By-Laws.
